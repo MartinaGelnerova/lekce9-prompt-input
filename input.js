04d27b5a67dec63a5805e3c6a9f752e44f6869d2
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-04-12 23:04:49 +0200

    vypracování úkolu 2

--- input.js
+++ input.js
@@ -1,12 +1,40 @@
 //ÚKOL 2 - input
-let A = parseFloat(document.querySelector('input[name="A"]').value);
-let B = parseFloat(document.querySelector('input[name="B"]').value);
 
 function secti () {
+  let A = parseFloat(document.querySelector('input[name="A"]').value);
+  let B = parseFloat(document.querySelector('input[name="B"]').value);
   let C = A + B;
-  return C;
+  document.querySelector('#soucet > span').innerHTML = C;
 }
 
-function sectiAVypis (secti) {
-  
+function odecti () {
+  let A = parseFloat(document.querySelector('input[name="A"]').value);
+  let B = parseFloat(document.querySelector('input[name="B"]').value);
+  let C = A - B;
+  document.querySelector('#rozdil > span').innerHTML = C;
+}
+
+function vynasob () {
+  let A = parseFloat(document.querySelector('input[name="A"]').value);
+  let B = parseFloat(document.querySelector('input[name="B"]').value);
+  let C = A * B;
+  document.querySelector('#soucin > span').innerHTML = C;
+}
+
+function vydel () {
+  let A = parseFloat(document.querySelector('input[name="A"]').value);
+  let B = parseFloat(document.querySelector('input[name="B"]').value);
+  let C = A / B;
+  document.querySelector('#podil > span').innerHTML = C;
+}
+
+document.querySelectorAll('input').forEach((element) => {
+  element.addEventListener("change", vypocitejAVypis);
+});
+
+function vypocitejAVypis() {
+  secti();
+  odecti();
+  vynasob();
+  vydel();
 }
