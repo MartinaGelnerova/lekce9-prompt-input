a01b4587b04ac03a5e4b1b6f7a88fea2d7e0ee5f
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-04-12 22:07:36 +0200

    oprava úkolu 1 z inputu na prompt

---
+++ input.js
@@ -0,0 +1,12 @@
+//ÚKOL 2 - input
+let A = parseFloat(document.querySelector('input[name="A"]').value);
+let B = parseFloat(document.querySelector('input[name="B"]').value);
+
+function secti () {
+  let C = A + B;
+  return C;
+}
+
+function sectiAVypis (secti) {
+  
+}
--- prompt.js
+++ prompt.js
@@ -1,23 +1,21 @@
 //ÚKOL 1 - ověření rozměrů pračky
-let vyskaMax = 70;
-let sirkaMax = 62;
-let hloubkaMax = 80;
+const vyskaMax = 70;
+const sirkaMax = 62;
+const hloubkaMax = 80;
 
+let vyska = prompt("Zadej VÝŠKU pračky v cm");
+let sirka = prompt("Zadej ŠÍŘKU pračky v cm");
+let hloubka = prompt("Zadej HLOUBKU pračky v cm");
+
+vypocitejVypisOvereni();
 
 function overRozmery() {
-  let vyska = parseFloat(document.querySelector('input[name="vyska"]').value);
-  let sirka = parseFloat(document.querySelector('input[name="sirka"]').value);
-  let hloubka = parseFloat(document.querySelector('input[name="hloubka"]').value);
   if (vyska > vyskaMax || sirka > sirkaMax || hloubka > hloubkaMax) {
     return "Bohužel, pračka se ti nevejde";
   } else {
-    return "OK";
+    return "OK - pračka se vejde";
   }
 }
-
-document.querySelectorAll('input').forEach((element) => {
-  element.addEventListener('change', vypocitejVypisOvereni);
-});
 
 function vypisVysledek(overeni) {
   document.querySelector('#vysledek > span').innerHTML = overeni;
